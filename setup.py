f2cb36999fa1dcc9621404a6e345587ef9385dc1
Michal <michal@maithu.com>
2015-08-26 14:26:51 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='vitadock-oauth',
+    version='0.1',
+    packages=['vitadock'],
+    url='https://github.com/gkotsis/vitadock-oauth',
+    license='Version 2.0',
+    author='George Gkotsis',
+    author_email='gkotsis@gmail.com',
+    description=''
+)
